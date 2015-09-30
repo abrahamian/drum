b87359af4ada4ea2c11c93ed9daa722be072d502
Mihran Abrahamian <abrahamian.mihran@gmail.com>
2015-09-30 15:12:01 -0400

    add user publication

---
+++ server/publications.js
@@ -0,0 +1,11 @@
+Meteor.publish("user", function(userId){
+  if (this.userId) {
+    if(this.userId == userId){
+      return Meteor.users.find({_id: userId});
+    } else {
+      return Meteor.users.find({_id: userId}, {fields: {'_id': 1, 'username':1} });
+    }
+  } else {
+    this.ready();
+  }
+});
