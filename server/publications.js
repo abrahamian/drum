9b2219d7e52cb0779b52e777c59f7cc2bf5bde2f
Mihran Abrahamian <abrahamian.mihran@gmail.com>
2015-10-19 15:04:02 -0400

    display username of logged in user, id of other users

--- server/publications.js
+++ server/publications.js
@@ -3,9 +3,10 @@ Meteor.publish("user", function(userId){
     if(this.userId == userId){
       return Meteor.users.find({_id: userId});
     } else {
-      return Meteor.users.find({_id: userId}, {fields: {'_id': 1, 'username':1} });
+      return Meteor.users.find({_id: userId}, {fields: {'_id': 1} });
     }
   } else {
     this.ready();
   }
+
 });
