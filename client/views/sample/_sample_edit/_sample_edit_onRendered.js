7fa72b9f9833d39156b0a23e5ef62080629af26d
Mihran Abrahamian <abrahamian.mihran@gmail.com>
2015-10-21 13:25:36 -0400

    user can update sample  data using region in waveform. sample region is reactive.

--- client/views/sample/_sample_edit/_sample_edit_onRendered.js
+++ client/views/sample/_sample_edit/_sample_edit_onRendered.js
@@ -20,20 +20,59 @@ Template._sample_edit.onRendered(function() {
   instance.wavesurfer.on('ready', function() {
     instance.wavesurfer.enableDragSelection();
 
+    var updateSample = function(sampleRegion) {
+      console.log('calling Sample/update');
+      Meteor.call(
+        'Sample/update',
+        instance.data.sample._id,
+        {
+          'startTime' : sampleRegion.start,
+          'duration': sampleRegion.end - sampleRegion.start
+        }
+      );
+    };
+
+    var initializeSampleRegion = function() {
+      instance.sampleRegion = instance.wavesurfer.addRegion({
+        start: instance.sample().startTime, 
+        end: instance.sample().startTime + instance.sample().duration,
+        id: "sample-region",
+        resize: true,
+      });
+      instance.sampleRegion.element.classList.add('sample');
+    };
+
+    var updateSampleRegion = function(sample) {
+      instance.sampleRegion.update({
+        start: sample.startTime,
+        end: sample.startTime + sample.duration 
+      });
+    };
+
     var replaceSampleRegion = function(newRegion) {
-      if(instance.sampleRegion){
-        instance.sampleRegion.remove();
-      }
+      instance.sampleRegion.remove();
       instance.sampleRegion = newRegion;
       instance.sampleRegion.element.classList.add('sample');
     };
 
+    initializeSampleRegion();
+
     instance.wavesurfer.on('region-created', function(newRegion) {
+      console.log('replacing sample region');
       replaceSampleRegion(newRegion);
     });
 
+    instance.wavesurfer.on('region-update-end', function(regionUpdated) {
+      if(regionUpdated == instance.sampleRegion){
+        updateSample(instance.sampleRegion);
+      }
+    });
+
+    instance.autorun(function(){
+      var dataContext = Template.currentData();
+      updateSampleRegion(dataContext.sample);
+    });
+
   });
-
-
 
 });
