9d525ca7520948b5cd6a704269fab5c187ed3f72
Mihran Abrahamian <abrahamian.mihran@gmail.com>
2015-10-22 18:29:40 -0400

    user can drag region borders to set fade durations.

--- client/views/sample/_sample_edit/_sample_edit_onRendered.js
+++ client/views/sample/_sample_edit/_sample_edit_onRendered.js
@@ -20,18 +20,6 @@ Template._sample_edit.onRendered(function() {
   instance.wavesurfer.on('ready', function() {
     instance.wavesurfer.enableDragSelection();
 
-    var updateSample = function(sampleRegion) {
-      console.log('calling Sample/update');
-      Meteor.call(
-        'Sample/update',
-        instance.data.sample._id,
-        {
-          'startTime' : sampleRegion.start,
-          'duration': sampleRegion.end - sampleRegion.start
-        }
-      );
-    };
-
     var initializeSampleRegion = function() {
       instance.sampleRegion = instance.wavesurfer.addRegion({
         start: instance.sample().startTime, 
@@ -40,6 +28,10 @@ Template._sample_edit.onRendered(function() {
         resize: true,
       });
       instance.sampleRegion.element.classList.add('sample');
+
+      instance.sampleRegion.duration = function(){
+        return this.end - this.start;
+      }
     };
 
     var updateSampleRegion = function(sample) {
@@ -53,24 +45,108 @@ Template._sample_edit.onRendered(function() {
       instance.sampleRegion.remove();
       instance.sampleRegion = newRegion;
       instance.sampleRegion.element.classList.add('sample');
+
+      instance.sampleRegion.duration = function(){
+        return this.end - this.start;
+      };
+    };
+
+    var initializeFadeInRegion = function(){
+      instance.fadeInRegion = instance.wavesurfer.addRegion({
+        start: instance.sample().startTime - instance.sample().fadeIn.duration,
+        end: instance.sample().startTime,
+        drag: false,
+        resize: true,
+        id: "fade-in-region",
+        color: "rgba(255, 192, 203,0.5)"        
+      });
+
+      instance.fadeInRegion.duration = function(){
+        return this.end - this.start;
+      };
+
+      instance.fadeInRegion.element.classList.add('fade');
+      instance.fadeInRegion.element.classList.add('fade-in');
+
+    };
+
+    var initializeFadeOutRegion = function(){
+      instance.fadeOutRegion = instance.wavesurfer.addRegion({
+        start: instance.sample().startTime + instance.sample().duration,
+        end: instance.sample().startTime + instance.sample().duration + instance.sample().fadeOut.duration,
+        drag: false,
+        resize: true,
+        id: "fade-out-region",
+        color: "rgba(255, 192, 203,0.5)"        
+      });
+
+      instance.fadeOutRegion.duration = function(){
+        return this.end - this.start;
+      };
+
+      instance.fadeInRegion.element.classList.add('fade');
+      instance.fadeOutRegion.element.classList.add('fade-out');
+    };
+
+    var updateBothFadeRegionsWithSampleRegionData = function() {
+      instance.fadeInRegion.update({
+        start: instance.sampleRegion.start - instance.fadeInRegion.duration(),
+        end: instance.sampleRegion.start
+      });
+
+      instance.fadeOutRegion.update({
+        start: instance.sampleRegion.end,
+        end: instance.sampleRegion.end + instance.fadeOutRegion.duration(),
+      });
+    };
+
+    var updateBothFadeRegionsWithSampleData = function(){
+      instance.fadeInRegion.update({
+        start: instance.sample().startTime - instance.sample().fadeIn.duration,
+        end: instance.sample().startTime
+      });
+
+      instance.fadeOutRegion.update({
+        start: instance.sample().startTime + instance.sample().duration,
+        end: instance.sample().startTime + instance.sample().duration + instance.sample().fadeOut.duration
+      });
+    }
+
+    var updateSampleFromRegions = function() {
+      Meteor.call(
+        'Sample/update',
+        instance.data.sample._id,
+        {
+          'startTime' : instance.sampleRegion.start,
+          'duration': instance.sampleRegion.end - instance.sampleRegion.start,
+          'fadeIn.duration' : (instance.fadeInRegion.end - instance.fadeInRegion.start),
+          'fadeOut.duration' : (instance.fadeOutRegion.end - instance.fadeOutRegion.start)
+        }
+      );
     };
 
     initializeSampleRegion();
+    initializeFadeInRegion();
+    initializeFadeOutRegion();
 
     instance.wavesurfer.on('region-created', function(newRegion) {
-      console.log('replacing sample region');
       replaceSampleRegion(newRegion);
     });
 
     instance.wavesurfer.on('region-update-end', function(regionUpdated) {
-      if(regionUpdated == instance.sampleRegion){
-        updateSample(instance.sampleRegion);
+      updateSampleFromRegions();
+    });
+
+    instance.wavesurfer.on('region-updated', function(region){
+      if( region == instance.sampleRegion ) {
+        updateBothFadeRegionsWithSampleData();
+        updateBothFadeRegionsWithSampleRegionData();
       }
     });
 
     instance.autorun(function(){
-      var dataContext = Template.currentData();
-      updateSampleRegion(dataContext.sample);
+      var sampleData = Template.currentData().sample;
+      updateSampleRegion(sampleData);
     });
 
   });
