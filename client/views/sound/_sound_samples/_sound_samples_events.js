2a537d9a63fd46c1697d3fc1ab0d2cf8d01fba0f
Mihran Abrahamian <abrahamian.mihran@gmail.com>
2015-10-27 17:39:59 -0400

    user can add/remove sounds and samples

--- client/views/sound/_sound_samples/_sound_samples_events.js
+++ client/views/sound/_sound_samples/_sound_samples_events.js
@@ -1,7 +1,32 @@
 Template._sound_samples.events({
+
+  'click .new-sample' : function(){
+    var soundId = this.soundId;
+    console.log(this);
+
+    Meteor.call(
+      'Samples/new', 
+      {soundId: soundId}, 
+      function(error, sampleId) {
+        if (error) {
+          // show a nice error message
+          alert(error.reason)
+        } else {
+          Session.set('selectedSampleId', sampleId);  
+        }
+      }
+    );
+  },
+
   'click li a' : function(event){
     event.preventDefault();
     var sample = this;
     Session.set('selectedSampleId', sample._id);
+  },
+
+  'click .remove-sample' : function(event, instance){    
+    var sampleId = this._id;
+    Meteor.call('Sample/remove', sampleId);
   }
+
 });
