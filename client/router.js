8b793173f6bf23838d48717894fa51b9a25d898f
Mihran Abrahamian <abrahamian.mihran@gmail.com>
2015-11-02 11:15:25 -0500

    add router

---
+++ client/router.js
@@ -0,0 +1,11 @@
+FlowRouter.route('/', {
+    name: 'drumRoute',
+    action: function(){}
+});
+
+// FlowRouter.route('/', {
+//   triggersEnter: [function(context, redirect) {
+//     redirect('/drum');
+//   }],
+//   action: function(){}
+// });
