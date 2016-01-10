2584af8fab71610c77f5a077eefdf8cba7527720
Mihran Abrahamian <abrahamian.mihran@gmail.com>
2016-01-09 22:57:10 -0500

    cache sounds locally and avoid multiple downloads of same sound

--- client/features/SampleLoader/SampleLoader_onCreated.js
+++ client/features/SampleLoader/SampleLoader_onCreated.js
@@ -1,5 +1,6 @@
 Template.SampleLoader.onCreated(function(){
   var instance = this;
+  window.SOUND_PROMISES = window.SOUND_PROMISES || {};
 
   var extractSample = function(tape, sample){
     return tape.slice(sample.startTime - sample.fades.in.duration, sample.duration + sample.fades.out.duration).render();
@@ -9,7 +10,13 @@ Template.SampleLoader.onCreated(function(){
     var sample = Template.currentData().sample();
     instance.subscribe('sound', sample.soundId, function(){
       var sound = Sounds.findOne(sample.soundId);
-      var soundPromise = sound.requestTape();
+      var soundPromise;
+      if(window.SOUND_PROMISES[sample.soundId]){
+        soundPromise = window.SOUND_PROMISES[sample.soundId];
+      } else {
+        soundPromise = sound.requestTape();
+        window.SOUND_PROMISES[sample.soundId] = soundPromise;
+      }
       soundPromise.then(function(tape){
         var samplePromise = extractSample(tape, sample);          
         samplePromise.then(function(sampleBuffer) {
